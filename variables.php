3194aa936ef3a41132a296bb39f51bf60165856a
MostafaAlii <mostafa0alii@gmail.com>
2022-11-10 19:35:30 +0200

    start with variables

---
+++ variables.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Variables must start with a letter or an underscore ( _ ) and cannot start with a number.
+ * Variables are case sensitive ($name and $NAME are two different variables).
+ * Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ).
+ */
+$name = "mostafa";
+echo $name;
