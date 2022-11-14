5874b7c9c55ca303807e9b03c4b32149c45a81b4
MostafaAlii <mostafa0alii@gmail.com>
2022-11-14 21:58:45 +0200

    start with string

--- datatypes.php
+++ datatypes.php
@@ -19,22 +19,63 @@
  * Special Data Types are: Resource and NULL
  */
 // String
+/*
+ * ٍString Notes:
+ * فى حاله ان هستخدم نصوص غفط لاغير يجب استخدام ' ' وليس " "
+ * فى حاله ان ههطبع قيممه متغير بداخل متغير اخر هستخدم الـ " " مثل
+ * $height = 1.75;
+ * echo "my height is {$height} meters <br>";
+*/
 $name = "Mostafa";
-echo $name;
-echo "<br>";
+echo "String Type " . $name;
+echo "<hr>";
 // Integer
+echo "String Type With Integer <br>";
 $age = 25;
-echo $age;
-echo "<br>";
+echo 'my age is ' . $age . ' years old and after  ' . $age . ' will be ' . 2 * $age . ' years old <br>';
+echo "max integer value is " . PHP_INT_MAX . "<br>";
+echo "min integer value is " . PHP_INT_MIN . "<br>";
+echo "max size of integer value is " . PHP_INT_SIZE . "<br>";
+echo "<hr>";
 // Float
+echo "Float Type <br>";
 $height = 1.75;
-echo $height;
-echo "<br>";
+echo "my height is {$height} meters <br>";
+echo 10e6 . "<br>";
+echo 4e5;
+echo "<hr>";
 // Boolean
+echo "Boolean Type True or False like 0 & 1  <br>";
 $isMale = true;
 echo $isMale;
-echo "<br>";
+echo "<hr>";
 // Array
+echo "Array Type ['mostafa', 'ali', 'ahmed'] <br>";
+echo "<p>
+    i have 3 friends <br>
+    1. mostafa <br>
+    2. ali <br>
+    3. ahmed <br>
+    we i need show this friends in array <br>
+    make print_r() function to show array <br>
+    like this: <br>
+</p>";
 $friends = array("Mostafa", "Ahmed", "Ali");
+print_r($friends);
+echo "<p>
+    but i want to print a specific friend <br>
+    now make echo with array name and index of friend <br>
+    like this: <br>
+    friends[0] <br>
+    friends[1] <br>
+    in friends[0] 0 is and index of frinds array and value of 0 is mostafa <br>
+    in friends[1] 1 is and index of frinds array and value of 1 is ahmed <br>
+    in friends[2] 2 is and index of frinds array and value of 2 is ali <br>
+    like this: <br>
+</p>";
 echo $friends[0];
 echo "<br>";
+echo $friends[1];
+echo "<br>";
+echo $friends[2];
+echo "<hr>";
